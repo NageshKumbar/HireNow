a58de362cd5ef5d6fd4e2ba7eeb2fb292a91cdfe
unknown <keerthan-gowda-c>
2026-05-06 15:42:33 +0530

    Candidate/Education Model inserted

--- Backend/candidate/models.py
+++ Backend/candidate/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
+from django.core.exceptions import ValidationError
+from ..utils.countryCode import *
+
+
 # Create your models here.
+class Candidate(models.Model):
+
+    class Gender(models.gender_choices):
+        MALE = 'male','Male'
+        FEMALE = 'female','Female'
+        OTHERS = 'others','Others'
+        PREFER_NOT_TO_SAY = 'prefer_not_to_say','Prefer Not To Say'
+
+
+    candidate_id = models.CharField(max_length=20, primary_key=True, unique=True, editable=False)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200) 
+    gender = models.CharField(max_length=10, choices = Gender.gender_choice, default=Gender.PREFER_NOT_TO_SAY)
+    mobile_number = models.CharField(max_length=15)
+    candidate_image = models.ImageField(upload_to='candidate_images/')
+    candidate_bio = models.CharField(max_length=200)
+    candidate_about = models.CharField(max_length=1500)
+    candidate_website = models.CharField(max_length=100)
+    added_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now= True)
+    is_active = models.BooleanField(default = True)
+
+    
+
+    def clean(self):
+        number = self.mobile_number.strip()
+
+        # Only digits allowed in mobile number field
+        if not number.isdigit():
+            raise ValidationError({
+                "mobile_number": "Enter digits only."
+            })
+
+        required_length = rules.get(self.country_code)
+
+        if required_length and len(number) != required_length:
+            raise ValidationError({
+                "mobile_number": f"For {self.country_code}, enter exactly {required_length} digits."
+            })
+
+    def save(self, *args, **kwargs):
+        self.full_clean()   # Run validation before saving
+        super().save(*args, **kwargs)
+
+
+
+    def save(self, *args, **kwargs):
+            if not self.candidate_id:
+                last_candidate = Candidate.objects.order_by('-id').first()
+
+                if last_candidate:
+                    last_id = int(last_candidate.candidate_id.replace('CAN', ''))
+                    new_id = last_id + 1
+                else:
+                    new_id = 1001
+
+                self.candidate_id = f"CAN{new_id}"
+
+            super().save(*args, **kwargs)
+
+    
+    
+class Education(models.Model):
+    candidate_id = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='educations')
+    institute_name = models.CharField(max_length=200)
+    degree = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    start_date = models.CharField(max_length=5)
+    end_date = models.CharField(max_length=5)
+    course = models.CharField(max_length=50)
+    class Grades(models.grade_choice):
+        CGPA = 'CGPA'
+        PERCENTAGE = 'Percentage'
+    grade = models.CharField(max_length=5, choices=Grades.grade_choice)
+    
---
+++ Backend/utils/countryCode.py
@@ -0,0 +1,425 @@
+
+country_choices = [
+        ("+93", "Afghanistan (+93)"),
+        ("+355", "Albania (+355)"),
+        ("+213", "Algeria (+213)"),
+        ("+1-684", "American Samoa (+1-684)"),
+        ("+376", "Andorra (+376)"),
+        ("+244", "Angola (+244)"),
+        ("+1-264", "Anguilla (+1-264)"),
+        ("+672", "Antarctica (+672)"),
+        ("+1-268", "Antigua and Barbuda (+1-268)"),
+        ("+54", "Argentina (+54)"),
+        ("+374", "Armenia (+374)"),
+        ("+297", "Aruba (+297)"),
+        ("+61", "Australia (+61)"),
+        ("+43", "Austria (+43)"),
+        ("+994", "Azerbaijan (+994)"),
+        ("+1-242", "Bahamas (+1-242)"),
+        ("+973", "Bahrain (+973)"),
+        ("+880", "Bangladesh (+880)"),
+        ("+1-246", "Barbados (+1-246)"),
+        ("+375", "Belarus (+375)"),
+        ("+32", "Belgium (+32)"),
+        ("+501", "Belize (+501)"),
+        ("+229", "Benin (+229)"),
+        ("+1-441", "Bermuda (+1-441)"),
+        ("+975", "Bhutan (+975)"),
+        ("+591", "Bolivia (+591)"),
+        ("+387", "Bosnia and Herzegovina (+387)"),
+        ("+267", "Botswana (+267)"),
+        ("+55", "Brazil (+55)"),
+        ("+246", "British Indian Ocean Territory (+246)"),
+        ("+673", "Brunei (+673)"),
+        ("+359", "Bulgaria (+359)"),
+        ("+226", "Burkina Faso (+226)"),
+        ("+257", "Burundi (+257)"),
+        ("+855", "Cambodia (+855)"),
+        ("+237", "Cameroon (+237)"),
+        ("+1", "Canada (+1)"),
+        ("+238", "Cape Verde (+238)"),
+        ("+1-345", "Cayman Islands (+1-345)"),
+        ("+236", "Central African Republic (+236)"),
+        ("+235", "Chad (+235)"),
+        ("+56", "Chile (+56)"),
+        ("+86", "China (+86)"),
+        ("+61", "Christmas Island (+61)"),
+        ("+57", "Colombia (+57)"),
+        ("+269", "Comoros (+269)"),
+        ("+242", "Congo (+242)"),
+        ("+243", "Congo DR (+243)"),
+        ("+682", "Cook Islands (+682)"),
+        ("+506", "Costa Rica (+506)"),
+        ("+385", "Croatia (+385)"),
+        ("+53", "Cuba (+53)"),
+        ("+357", "Cyprus (+357)"),
+        ("+420", "Czech Republic (+420)"),
+        ("+45", "Denmark (+45)"),
+        ("+253", "Djibouti (+253)"),
+        ("+1-767", "Dominica (+1-767)"),
+        ("+1-809", "Dominican Republic (+1-809)"),
+        ("+593", "Ecuador (+593)"),
+        ("+20", "Egypt (+20)"),
+        ("+503", "El Salvador (+503)"),
+        ("+240", "Equatorial Guinea (+240)"),
+        ("+291", "Eritrea (+291)"),
+        ("+372", "Estonia (+372)"),
+        ("+251", "Ethiopia (+251)"),
+        ("+500", "Falkland Islands (+500)"),
+        ("+298", "Faroe Islands (+298)"),
+        ("+679", "Fiji (+679)"),
+        ("+358", "Finland (+358)"),
+        ("+33", "France (+33)"),
+        ("+689", "French Polynesia (+689)"),
+        ("+241", "Gabon (+241)"),
+        ("+220", "Gambia (+220)"),
+        ("+995", "Georgia (+995)"),
+        ("+49", "Germany (+49)"),
+        ("+233", "Ghana (+233)"),
+        ("+350", "Gibraltar (+350)"),
+        ("+30", "Greece (+30)"),
+        ("+299", "Greenland (+299)"),
+        ("+1-473", "Grenada (+1-473)"),
+        ("+590", "Guadeloupe (+590)"),
+        ("+1-671", "Guam (+1-671)"),
+        ("+502", "Guatemala (+502)"),
+        ("+44", "Guernsey (+44)"),
+        ("+224", "Guinea (+224)"),
+        ("+245", "Guinea-Bissau (+245)"),
+        ("+592", "Guyana (+592)"),
+        ("+509", "Haiti (+509)"),
+        ("+504", "Honduras (+504)"),
+        ("+852", "Hong Kong (+852)"),
+        ("+36", "Hungary (+36)"),
+        ("+354", "Iceland (+354)"),
+        ("+91", "India (+91)"),
+        ("+62", "Indonesia (+62)"),
+        ("+98", "Iran (+98)"),
+        ("+964", "Iraq (+964)"),
+        ("+353", "Ireland (+353)"),
+        ("+44", "Isle of Man (+44)"),
+        ("+972", "Israel (+972)"),
+        ("+39", "Italy (+39)"),
+        ("+225", "Ivory Coast (+225)"),
+        ("+1-876", "Jamaica (+1-876)"),
+        ("+81", "Japan (+81)"),
+        ("+44", "Jersey (+44)"),
+        ("+962", "Jordan (+962)"),
+        ("+7", "Kazakhstan (+7)"),
+        ("+254", "Kenya (+254)"),
+        ("+686", "Kiribati (+686)"),
+        ("+965", "Kuwait (+965)"),
+        ("+996", "Kyrgyzstan (+996)"),
+        ("+856", "Laos (+856)"),
+        ("+371", "Latvia (+371)"),
+        ("+961", "Lebanon (+961)"),
+        ("+266", "Lesotho (+266)"),
+        ("+231", "Liberia (+231)"),
+        ("+218", "Libya (+218)"),
+        ("+423", "Liechtenstein (+423)"),
+        ("+370", "Lithuania (+370)"),
+        ("+352", "Luxembourg (+352)"),
+        ("+853", "Macau (+853)"),
+        ("+389", "North Macedonia (+389)"),
+        ("+261", "Madagascar (+261)"),
+        ("+265", "Malawi (+265)"),
+        ("+60", "Malaysia (+60)"),
+        ("+960", "Maldives (+960)"),
+        ("+223", "Mali (+223)"),
+        ("+356", "Malta (+356)"),
+        ("+692", "Marshall Islands (+692)"),
+        ("+596", "Martinique (+596)"),
+        ("+222", "Mauritania (+222)"),
+        ("+230", "Mauritius (+230)"),
+        ("+262", "Mayotte (+262)"),
+        ("+52", "Mexico (+52)"),
+        ("+691", "Micronesia (+691)"),
+        ("+373", "Moldova (+373)"),
+        ("+377", "Monaco (+377)"),
+        ("+976", "Mongolia (+976)"),
+        ("+382", "Montenegro (+382)"),
+        ("+1-664", "Montserrat (+1-664)"),
+        ("+212", "Morocco (+212)"),
+        ("+258", "Mozambique (+258)"),
+        ("+95", "Myanmar (+95)"),
+        ("+264", "Namibia (+264)"),
+        ("+674", "Nauru (+674)"),
+        ("+977", "Nepal (+977)"),
+        ("+31", "Netherlands (+31)"),
+        ("+687", "New Caledonia (+687)"),
+        ("+64", "New Zealand (+64)"),
+        ("+505", "Nicaragua (+505)"),
+        ("+227", "Niger (+227)"),
+        ("+234", "Nigeria (+234)"),
+        ("+683", "Niue (+683)"),
+        ("+850", "North Korea (+850)"),
+        ("+47", "Norway (+47)"),
+        ("+968", "Oman (+968)"),
+        ("+92", "Pakistan (+92)"),
+        ("+680", "Palau (+680)"),
+        ("+970", "Palestine (+970)"),
+        ("+507", "Panama (+507)"),
+        ("+675", "Papua New Guinea (+675)"),
+        ("+595", "Paraguay (+595)"),
+        ("+51", "Peru (+51)"),
+        ("+63", "Philippines (+63)"),
+        ("+48", "Poland (+48)"),
+        ("+351", "Portugal (+351)"),
+        ("+1-787", "Puerto Rico (+1-787)"),
+        ("+974", "Qatar (+974)"),
+        ("+40", "Romania (+40)"),
+        ("+7", "Russia (+7)"),
+        ("+250", "Rwanda (+250)"),
+        ("+966", "Saudi Arabia (+966)"),
+        ("+221", "Senegal (+221)"),
+        ("+381", "Serbia (+381)"),
+        ("+248", "Seychelles (+248)"),
+        ("+232", "Sierra Leone (+232)"),
+        ("+65", "Singapore (+65)"),
+        ("+421", "Slovakia (+421)"),
+        ("+386", "Slovenia (+386)"),
+        ("+677", "Solomon Islands (+677)"),
+        ("+252", "Somalia (+252)"),
+        ("+27", "South Africa (+27)"),
+        ("+82", "South Korea (+82)"),
+        ("+34", "Spain (+34)"),
+        ("+94", "Sri Lanka (+94)"),
+        ("+249", "Sudan (+249)"),
+        ("+597", "Suriname (+597)"),
+        ("+46", "Sweden (+46)"),
+        ("+41", "Switzerland (+41)"),
+        ("+963", "Syria (+963)"),
+        ("+886", "Taiwan (+886)"),
+        ("+992", "Tajikistan (+992)"),
+        ("+255", "Tanzania (+255)"),
+        ("+66", "Thailand (+66)"),
+        ("+228", "Togo (+228)"),
+        ("+676", "Tonga (+676)"),
+        ("+1-868", "Trinidad and Tobago (+1-868)"),
+        ("+216", "Tunisia (+216)"),
+        ("+90", "Turkey (+90)"),
+        ("+993", "Turkmenistan (+993)"),
+        ("+1-649", "Turks and Caicos (+1-649)"),
+        ("+688", "Tuvalu (+688)"),
+        ("+256", "Uganda (+256)"),
+        ("+380", "Ukraine (+380)"),
+        ("+971", "UAE (+971)"),
+        ("+44", "United Kingdom (+44)"),
+        ("+1", "United States (+1)"),
+        ("+598", "Uruguay (+598)"),
+        ("+998", "Uzbekistan (+998)"),
+        ("+678", "Vanuatu (+678)"),
+        ("+58", "Venezuela (+58)"),
+        ("+84", "Vietnam (+84)"),
+        ("+967", "Yemen (+967)"),
+        ("+260", "Zambia (+260)"),
+        ("+263", "Zimbabwe (+263)"),
+    ]
+
+
+# Rules for each country_code
+# country_mobile_rules.py
+
+rules = {
+    "+93": 9,     # Afghanistan
+    "+355": 9,    # Albania
+    "+213": 9,    # Algeria
+    "+1-684": 10, # American Samoa
+    "+376": 6,    # Andorra
+    "+244": 9,    # Angola
+    "+1-264": 10, # Anguilla
+    "+672": 6,    # Antarctica
+    "+1-268": 10, # Antigua and Barbuda
+    "+54": 10,    # Argentina
+    "+374": 8,    # Armenia
+    "+297": 7,    # Aruba
+    "+61": 9,     # Australia
+    "+43": 10,    # Austria
+    "+994": 9,    # Azerbaijan
+    "+1-242": 10, # Bahamas
+    "+973": 8,    # Bahrain
+    "+880": 10,   # Bangladesh
+    "+1-246": 10, # Barbados
+    "+375": 9,    # Belarus
+    "+32": 9,     # Belgium
+    "+501": 7,    # Belize
+    "+229": 8,    # Benin
+    "+1-441": 10, # Bermuda
+    "+975": 8,    # Bhutan
+    "+591": 8,    # Bolivia
+    "+387": 8,    # Bosnia and Herzegovina
+    "+267": 8,    # Botswana
+    "+55": 11,    # Brazil
+    "+673": 7,    # Brunei
+    "+359": 9,    # Bulgaria
+    "+226": 8,    # Burkina Faso
+    "+257": 8,    # Burundi
+    "+855": 9,    # Cambodia
+    "+237": 9,    # Cameroon
+    "+1": 10,     # Canada / USA
+    "+238": 7,    # Cape Verde
+    "+1-345": 10, # Cayman Islands
+    "+236": 8,    # Central African Republic
+    "+235": 8,    # Chad
+    "+56": 9,     # Chile
+    "+86": 11,    # China
+    "+57": 10,    # Colombia
+    "+269": 7,    # Comoros
+    "+242": 9,    # Congo
+    "+243": 9,    # Congo DR
+    "+682": 5,    # Cook Islands
+    "+506": 8,    # Costa Rica
+    "+385": 9,    # Croatia
+    "+53": 8,     # Cuba
+    "+357": 8,    # Cyprus
+    "+420": 9,    # Czech Republic
+    "+45": 8,     # Denmark
+    "+253": 8,    # Djibouti
+    "+1-767": 10, # Dominica
+    "+1-809": 10, # Dominican Republic
+    "+593": 9,    # Ecuador
+    "+20": 10,    # Egypt
+    "+503": 8,    # El Salvador
+    "+240": 9,    # Equatorial Guinea
+    "+291": 7,    # Eritrea
+    "+372": 8,    # Estonia
+    "+251": 9,    # Ethiopia
+    "+679": 7,    # Fiji
+    "+358": 10,   # Finland
+    "+33": 9,     # France
+    "+241": 7,    # Gabon
+    "+220": 7,    # Gambia
+    "+995": 9,    # Georgia
+    "+49": 10,    # Germany
+    "+233": 9,    # Ghana
+    "+350": 8,    # Gibraltar
+    "+30": 10,    # Greece
+    "+299": 6,    # Greenland
+    "+1-473": 10, # Grenada
+    "+590": 9,    # Guadeloupe
+    "+1-671": 10, # Guam
+    "+502": 8,    # Guatemala
+    "+224": 9,    # Guinea
+    "+245": 7,    # Guinea-Bissau
+    "+592": 7,    # Guyana
+    "+509": 8,    # Haiti
+    "+504": 8,    # Honduras
+    "+852": 8,    # Hong Kong
+    "+36": 9,     # Hungary
+    "+354": 7,    # Iceland
+    "+91": 10,    # India
+    "+62": 10,    # Indonesia
+    "+98": 10,    # Iran
+    "+964": 10,   # Iraq
+    "+353": 9,    # Ireland
+    "+972": 9,    # Israel
+    "+39": 10,    # Italy
+    "+225": 10,   # Ivory Coast
+    "+1-876": 10, # Jamaica
+    "+81": 10,    # Japan
+    "+962": 9,    # Jordan
+    "+7": 10,     # Kazakhstan / Russia
+    "+254": 9,    # Kenya
+    "+686": 5,    # Kiribati
+    "+965": 8,    # Kuwait
+    "+996": 9,    # Kyrgyzstan
+    "+856": 8,    # Laos
+    "+371": 8,    # Latvia
+    "+961": 8,    # Lebanon
+    "+266": 8,    # Lesotho
+    "+231": 7,    # Liberia
+    "+218": 9,    # Libya
+    "+423": 7,    # Liechtenstein
+    "+370": 8,    # Lithuania
+    "+352": 9,    # Luxembourg
+    "+853": 8,    # Macau
+    "+389": 8,    # North Macedonia
+    "+261": 9,    # Madagascar
+    "+265": 9,    # Malawi
+    "+60": 9,     # Malaysia
+    "+960": 7,    # Maldives
+    "+223": 8,    # Mali
+    "+356": 8,    # Malta
+    "+692": 7,    # Marshall Islands
+    "+596": 9,    # Martinique
+    "+222": 8,    # Mauritania
+    "+230": 8,    # Mauritius
+    "+52": 10,    # Mexico
+    "+691": 7,    # Micronesia
+    "+373": 8,    # Moldova
+    "+377": 8,    # Monaco
+    "+976": 8,    # Mongolia
+    "+382": 8,    # Montenegro
+    "+1-664": 10, # Montserrat
+    "+212": 9,    # Morocco
+    "+258": 9,    # Mozambique
+    "+95": 9,     # Myanmar
+    "+264": 9,    # Namibia
+    "+674": 7,    # Nauru
+    "+977": 10,   # Nepal
+    "+31": 9,     # Netherlands
+    "+64": 9,     # New Zealand
+    "+505": 8,    # Nicaragua
+    "+227": 8,    # Niger
+    "+234": 10,   # Nigeria
+    "+850": 10,   # North Korea
+    "+47": 8,     # Norway
+    "+968": 8,    # Oman
+    "+92": 10,    # Pakistan
+    "+680": 7,    # Palau
+    "+970": 9,    # Palestine
+    "+507": 8,    # Panama
+    "+675": 8,    # Papua New Guinea
+    "+595": 9,    # Paraguay
+    "+51": 9,     # Peru
+    "+63": 10,    # Philippines
+    "+48": 9,     # Poland
+    "+351": 9,    # Portugal
+    "+1-787": 10, # Puerto Rico
+    "+974": 8,    # Qatar
+    "+40": 9,     # Romania
+    "+250": 9,    # Rwanda
+    "+966": 9,    # Saudi Arabia
+    "+221": 9,    # Senegal
+    "+381": 9,    # Serbia
+    "+248": 7,    # Seychelles
+    "+232": 8,    # Sierra Leone
+    "+65": 8,     # Singapore
+    "+421": 9,    # Slovakia
+    "+386": 8,    # Slovenia
+    "+677": 5,    # Solomon Islands
+    "+252": 8,    # Somalia
+    "+27": 9,     # South Africa
+    "+82": 10,    # South Korea
+    "+34": 9,     # Spain
+    "+94": 9,     # Sri Lanka
+    "+249": 9,    # Sudan
+    "+597": 7,    # Suriname
+    "+46": 9,     # Sweden
+    "+41": 9,     # Switzerland
+    "+963": 9,    # Syria
+    "+886": 9,    # Taiwan
+    "+992": 9,    # Tajikistan
+    "+255": 9,    # Tanzania
+    "+66": 9,     # Thailand
+    "+228": 8,    # Togo
+    "+676": 5,    # Tonga
+    "+1-868": 10, # Trinidad and Tobago
+    "+216": 8,    # Tunisia
+    "+90": 10,    # Turkey
+    "+993": 8,    # Turkmenistan
+    "+1-649": 10, # Turks and Caicos
+    "+688": 5,    # Tuvalu
+    "+256": 9,    # Uganda
+    "+380": 9,    # Ukraine
+    "+971": 9,    # UAE
+    "+44": 10,    # UK
+    "+598": 8,    # Uruguay
+    "+998": 9,    # Uzbekistan
+    "+678": 7,    # Vanuatu
+    "+58": 10,    # Venezuela
+    "+84": 9,     # Vietnam
+    "+967": 9,    # Yemen
+    "+260": 9,    # Zambia
+    "+263": 9,    # Zimbabwe
+}
